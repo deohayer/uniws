e6c7c7f234bb3fc0ec8839964ea289886848f0f8
Volodymyr Zakalyk <volodymyr.zakalyk@gmail.com>
2023-12-27 22:16:03 +0200

    Add entry points
    These functions are entry points for scripts defined in pyproject.toml.
    The function name matches the script name that will be available to the
    user.

---
+++ uniws.py
@@ -0,0 +1,113 @@
+# PYTHON_ARGCOMPLETE_OK
+
+#
+# Entry points.
+#
+
+
+def uniws():
+    '''
+    The primary application.
+    '''
+
+
+def uha():
+    '''
+    Shortcut: uniws hw attach.
+    '''
+
+
+def uhd():
+    '''
+    Shortcut: uniws hw detach.
+    '''
+
+
+def uho():
+    '''
+    Shortcut: uniws hw off.
+    '''
+
+
+def uhb():
+    '''
+    Shortcut: uniws hw boot.
+    '''
+
+
+def uhs():
+    '''
+    Shortcut: uniws hw shell.
+    '''
+
+
+def uhg():
+    '''
+    Shortcut: uniws hw get.
+    '''
+
+
+def uhp():
+    '''
+    Shortcut: uniws hw put.
+    '''
+
+
+def uhw():
+    '''
+    Shortcut: uniws hw watch.
+    '''
+
+
+def usf():
+    '''
+    Shortcut: uniws sw fetch.
+    '''
+
+
+def usp():
+    '''
+    Shortcut: uniws sw patch.
+    '''
+
+
+def usb():
+    '''
+    Shortcut: uniws sw build.
+    '''
+
+
+def usi():
+    '''
+    Shortcut: uniws sw install.
+    '''
+
+
+def ust():
+    '''
+    Shortcut: uniws sw test.
+    '''
+
+
+def usd():
+    '''
+    Shortcut: uniws sw deploy.
+    '''
+
+
+def usc():
+    '''
+    Shortcut: uniws sw clean.
+    '''
+
+
+def usr():
+    '''
+    Shortcut: uniws sw reset.
+    '''
+
+
+def usw():
+    '''
+    Shortcut: uniws sw wipe.
+    '''
